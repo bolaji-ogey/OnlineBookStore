80067e03e40ed2f4f5ff11c104aa43aa008da80f
Bolaji Ogeyingbo <bogeyingbo@gmail.com>
2024-09-08 05:31:06 +0100

    'updated'

--- src/main/java/i/ogeyingbo/trxn/compute/module/BillingChargeConfig.java
+++ src/main/java/i/ogeyingbo/trxn/compute/module/BillingChargeConfig.java
@@ -15,7 +15,7 @@ public class BillingChargeConfig {
     private   long  id   = -1;
     
     private   String  billingCode;
-    private   String  billingReference;
+    private   String  partnerCode;
     private   boolean  isActive = false;
     private   String  serviceId; 
     
@@ -31,7 +31,7 @@ public class BillingChargeConfig {
     private   BigDecimal  percentageOrFixedValue =  new  BigDecimal(0.00);
     private   BigDecimal  trxnChargeCap =  new  BigDecimal(0.00);
     
-    private   String      dateCOnfigured; 
+    private   String      dateConfigured; 
     
     private   boolean     usePercentageForTax  =  false; 
     private   BigDecimal  taxPercentageOrFixedValue =  new  BigDecimal(0.00);
@@ -224,13 +224,249 @@ public class BillingChargeConfig {
       
     
      
+    public  void   setId(long inId){
+        id = inId;
+    } 
+    
+    
+    public  void  setBillingCode(String  inBillingCode){
+        billingCode  =  inBillingCode;
+    }
+     
+    public  void  setPartnerCode(String  inPartnerCode){
+        partnerCode  =  inPartnerCode;
+    }
+    
+    public  void  setIsActive(boolean  inIsActive){
+        isActive  =  inIsActive;
+    }
+    
+    public  void  setServiceId(String  inServiceId){
+        serviceId  =  inServiceId;
+    }
+   
+    public  void  setServiceName(String  inServiceName){
+        serviceName  =  inServiceName;
+    }
+    
+    
+    public  void  setSchemeCode(String  inSchemeCode){
+        schemeCode  =  inSchemeCode;
+    }
+    
+    
+    public  void  setAppTrxnType(String  inAppTrxnType){
+        appTrxnType  =  inAppTrxnType;
+    }
+    
+    public  void  setTrxnBand(String  inTrxnBand){
+        trxnBand  =  inTrxnBand;
+    }
+    
+      
+    public  void  setLowerLimitValue(BigDecimal  inLowerLimitValue){
+        lowerLimitValue  =  inLowerLimitValue;
+    }
+    
+    public  void  setUpperLimitValue(BigDecimal  inUpperLimitValue){
+        upperLimitValue  =  inUpperLimitValue;
+    }
+    
+    public  void  setUsePercentage(boolean  inUsePercentage){
+        usePercentage  =  inUsePercentage;
+    }
+    
+    public  void  setPercentageOrFixedValue(BigDecimal  inPercentageOrFixedValue){
+        percentageOrFixedValue  =  inPercentageOrFixedValue;
+    }
+    
+    public  void  setTrxnChargeCap(BigDecimal  inTrxnChargeCap){
+        trxnChargeCap  =  inTrxnChargeCap;
+    }
+    
+    public  void  setDateConfigured(String  inDateConfigured){
+        dateConfigured  =  inDateConfigured;
+    }
+    
+    
+    
+    public  void  setUsePercentageForTax(boolean  inUsePercentageForTax){
+        usePercentageForTax  =  inUsePercentageForTax;
+    }
+    
+    public  void  setTaxPercentageOrFixedValue(BigDecimal  inTaxPercentageOrFixedValue){
+        taxPercentageOrFixedValue  =  inTaxPercentageOrFixedValue;
+    }
+    
+    public  void  setTaxChargeCap(BigDecimal  inTaxChargeCap){
+        taxChargeCap  =  inTaxChargeCap;
+    }
+    
+     
+    
+    public  void  setUsePercentageForBankCommission(boolean  inUsePercentageForBankCommission){
+        usePercentageForBankCommission  =  inUsePercentageForBankCommission;
+    }
+    
+    public  void  setBankCommissionPercentageOrFixedValue(BigDecimal  inBankCommissionPercentageOrFixedValue){
+        bankCommissionPercentageOrFixedValue  =  inBankCommissionPercentageOrFixedValue;
+    }
+    
+    public  void  setBankCommissionShareCap(BigDecimal  inBankCommissionShareCap){
+        bankCommissionShareCap  =  inBankCommissionShareCap;
+    } 
+    
+    
+    public  void  setUsePercentageForPartnerCommission(boolean  inUsePercentageForPartnerCommission){
+        usePercentageForPartnerCommission  =  inUsePercentageForPartnerCommission;
+    }
+    
+    public  void  setPartnerCommissionPercentageOrFixedValue(BigDecimal  inPartnerCommissionPercentageOrFixedValue){
+        partnerCommissionPercentageOrFixedValue  =  inPartnerCommissionPercentageOrFixedValue;
+    }
+    
+    public  void  setPartnerCommissionShareCap(BigDecimal  inPartnerCommissionShareCap){
+        partnerCommissionShareCap  =  inPartnerCommissionShareCap;
+    }
+    
+    
+    public  void  setBonusShare(BigDecimal  inBonusShare){
+        bonusShare  =  inBonusShare;
+    }
+    
+    
+    public  void  setBonusAccelerate(boolean  inBonusAccelerate){
+        bonusAccelerate  =  inBonusAccelerate;
+    }
      
      
-     
-     
+    
+    
      
          
          
+    
+    
+    
+    
+     
+    public  long   getId(long inId){
+       return    id = inId;
+    } 
+    
+    
+    public  String  getBillingCode(){
+        return   billingCode;
+    }
+     
+    public  String  getPartnerCode(){
+        return   partnerCode;
+    }
+    
+    public  boolean  getIsActive(){
+        return   isActive;
+    }
+    
+    public  String  getServiceId(){
+        return   serviceId;
+    }
+   
+    public  String  getServiceName(){
+        return   serviceName;
+    }
+    
+    
+    public  String  getSchemeCode(){
+       return    schemeCode;
+    }
+    
+    
+    public  String  getAppTrxnType(){
+        return   appTrxnType;
+    }
+    
+    public  String  getTrxnBand(){
+        return   trxnBand;
+    }
+    
+      
+    public  BigDecimal  getLowerLimitValue(){
+        return   lowerLimitValue;
+    }
+    
+    public  BigDecimal  getUpperLimitValue(){
+        return   upperLimitValue;
+    }
+    
+    public  boolean  getUsePercentage(){
+        return   usePercentage;
+    }
+    
+    public  BigDecimal  getPercentageOrFixedValue(){
+        return   percentageOrFixedValue;
+    }
+    
+    public  BigDecimal  getTrxnChargeCap(){
+        return   trxnChargeCap;
+    }
+    
+    public  String  getDateConfigured(){
+        return   dateConfigured;
+    }
+    
+    
+    
+    public  boolean  getUsePercentageForTax(){
+        return   usePercentageForTax;
+    }
+    
+    public  BigDecimal  getTaxPercentageOrFixedValue(){
+        return   taxPercentageOrFixedValue;
+    }
+    
+    public  BigDecimal  getTaxChargeCap(){
+        return   taxChargeCap;
+    }
+    
+     
+    
+    public  boolean  getUsePercentageForBankCommission(){
+        return   usePercentageForBankCommission;
+    }
+    
+    public  BigDecimal  getBankCommissionPercentageOrFixedValue(){
+        return   bankCommissionPercentageOrFixedValue;
+    }
+    
+    public  BigDecimal  getBankCommissionShareCap(){
+       return    bankCommissionShareCap;
+    } 
+    
+    
+    public  boolean  getUsePercentageForPartnerCommission(){
+       return    usePercentageForPartnerCommission;
+    }
+    
+    public  BigDecimal  getPartnerCommissionPercentageOrFixedValue(){
+       return    partnerCommissionPercentageOrFixedValue;
+    }
+    
+    public  BigDecimal  getPartnerCommissionShareCap(){
+       return    partnerCommissionShareCap;
+    }
+    
+    
+    public  BigDecimal  getBonusShare(){
+        return   bonusShare;
+    }
+    
+    
+    public  boolean  getBonusAccelerate(){
+        return   bonusAccelerate;
+    }
+    
+    
+    
          
     
 }
